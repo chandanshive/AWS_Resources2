3721aa887ef371cdde01567d06b9ee48272541e0
Accurics <admin@accurics.com>
2021-09-01 09:57:25 +0530

    Auto Remediation by Accurics, file: main.tf, resource: aws_s3_bucket.accuricsbucketdemo, violation: [HIGH]: Ensure at-rest server side encryption (SSE) is enabled for AWS S3 Buckets

--- main.tf
+++ main.tf
@@ -27,6 +27,14 @@ resource "aws_s3_bucket" "accuricsbucketdemo" {
   versioning {
     enabled = true
   }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
 
 resource "aws_vpc" "<resource_name>" {
